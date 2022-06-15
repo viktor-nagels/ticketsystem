aa78625dcef37f0452f0660e403e8c2fb419302f
Viktor Nagels <71647271+viktor-nagels@users.noreply.github.com>
2022-06-15 16:56:26 +0200

    csv without blank spaces

--- random id generator.py
+++ random id generator.py
@@ -1,15 +1,17 @@
 # Viktor Nagels
 import uuid
 import csv
+import sys
 
+output = ""
 length_uuid = 16
-total = 20
+total = 50000
 
-with open('uuid.txt', 'w', encoding='UTF8') as csvfile:
-    fieldnames = ['id', 'uuid']
+with open('uuid.csv', 'w', encoding='UTF8', newline="") as csvfile:
+    fieldnames = ['uuid']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
     for i in range(0, total):
         writer.writerow(
-            {'id': i, 'uuid': uuid.uuid4().hex[:length_uuid].upper()})
+            {'uuid': uuid.uuid4().hex[:length_uuid].upper()})
