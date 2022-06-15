2c0a2c6d9755cdd13af91a1c5941619eb2da0084
Viktor Nagels <71647271+viktor-nagels@users.noreply.github.com>
2022-06-15 12:01:25 +0200

    barcode inplement

--- qr code generator.py
+++ qr code generator.py
@@ -1,6 +1,8 @@
 # Viktor Nagels
 import qrcode
-
+from barcode import EAN13
+# ?barcode as png
+from barcode.writer import ImageWriter
 
 qr = qrcode.QRCode(
     version=7,
@@ -9,9 +11,20 @@ qr = qrcode.QRCode(
     border=4,
 )
 
+# TODO data given in a qr code:
+# qrcode data formaat = weblink/sublink/qrcode-unique id
+
+
 data = input(str("qr code information: "))
 
 qr.add_data(data)
+
+# ?barcode as png
+barcode = EAN13(data, writer=ImageWriter())
+
+# ?barcode as svg
+#barcode = EAN13(date)
+barcode.save('barcode')
 
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("qr.png")
